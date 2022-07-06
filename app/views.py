879d8fd2b5c0df3ab0eed2aaf3b81477b875c220
Felipe Silva <ffelipe.ssilva02@gmail.com>
2022-07-06 19:23:18 -0300

    video 5 update e create

--- app/views.py
+++ app/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from app.forms import PokemonForm
+from app.models import Pokemon
 
 # Create your views here.
 def home(request):
     data={}
-    data['pokemon']='Rildfglaboom'
+    data['db']=Pokemon.objects.all()
     return render(request,'index.html',data)
 
 
@@ -18,6 +19,25 @@ def form(request):
 
 def create(request):
     form = PokemonForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data={}
+    data['db']=Pokemon.objects.get(pk=pk)
+    return render(request,'view.html', data)
+
+def edit(request, pk):
+    data={}
+    data['db']=Pokemon.objects.get(pk=pk)
+    data['form']=PokemonForm(instance=data['db'])
+    return render(request,'formulario.html', data)
+
+def update(request, pk):
+    data={}
+    data['db']=Pokemon.objects.get(pk=pk)
+    form=PokemonForm(request.POST or None, instance=data['db'])
     if form.is_valid():
         form.save()
         return redirect('home')
